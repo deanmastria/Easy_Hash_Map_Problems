29faeaf7b7a6a0108d3f5dab90df0ac53599f40e
deanmastria <mastriadata@gmail.com>
2024-08-18 22:07:42 -0400

    Initial commit

---
+++ src/test/java/org/example/FrequencyCounterTest.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import org.junit.jupiter.api.test;
+import static org.juni.jupiter.api.Assertions.assertEquals;
+import java.util.HashMap;
+
+public class testFrequencyCounter {
+}
